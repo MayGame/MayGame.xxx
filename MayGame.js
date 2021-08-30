851d50b163bad883cffebe936fa83dbf16412234
Maygician <maygicinc@gmail.com>
2021-08-30 14:09:41 +0900

    life text

--- MayGame.js
+++ MayGame.js
@@ -2,7 +2,8 @@ let life = localStorage.getItem('life');
 if(!life){
     word = window.prompt("Word?", "Love");
 let create_life = async function(word) {
-    life = await fetch(word)
+    life = await fetch(word);
+    life = await life.text();
 }
 create_life(word);
 }
